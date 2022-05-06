6a63043f956288200a49c1760a3a8c3a7f57f77a
Getaji <t.tanaka.onion@gmail.com>
2022-05-06 16:56:48 +0900

    パスエイリアスの解決を修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      "@/": `${__dirname}/src/`
+    }
+  }
 })
